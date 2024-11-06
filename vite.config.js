0f571ac8aeadc03d95f5cf32f2cab77dbf47fa96
TranCongViet <trancongviet990ntt@gmail.com>
2024-11-06 21:36:01 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/jwt-authentication/",
   plugins: [react()],
   server: {
     port: 5173, // Thay đổi port tại đây
